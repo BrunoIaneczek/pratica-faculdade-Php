85b8db42fdcaac66e6ea5715fc29bb8f816578b2
Bruno Amaral Ianeczek <87914395+BrunoIaneczek@users.noreply.github.com>
2023-03-13 20:53:31 -0300

    Create unidade1_tp3_atividade1.php

---
+++ multiplica/unidade1_tp3_atividade1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br
+">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atividade 3 array</title>
+</head>
+<body>
+    <h1>Criando um array com a palavra "Uniasselvi"</h1>
+    <?php
+        
+        // $ sudo ./manager-linux-x64.run
+
+        $arrayUniasselvi = array("U","N","A","S","S","E","L","V","I")
+        print_r($arrayUniasselvi)
+    ?>
+</body>
+</html>
